8baf6e3d99b528043cfa735717d3cc1e08362507
Hansen Johnson <hansen.johnson@dal.ca>
2017-09-12 01:11:19 -0300

    consolidate helper and app scripts

--- app.R
+++ app.R
@@ -4,7 +4,6 @@ library(shiny)
 library(htmltools)
 library(htmlwidgets)
 library(oce)
-source('helper.R')
 
 # setup -------------------------------------------------------------------
 
@@ -19,6 +18,53 @@ glider_list = gsub(glider_list, pattern = 'data-', replacement = '')
 spp_choices = c('right', 'fin', 'sei', 'humpback')
 
 col_choices = c('red', 'lightgrey', 'darkgrey', 'tan', 'white', 'brown', 'yellow', 'lightblue', 'cyan', 'lightgreen', 'darkred', 'purple','green', 'black')
+
+
+# functions ---------------------------------------------------------------
+
+plot_section = function(ctd,var,zlim,cex.all=1.5){
+  
+  # switch for variable
+  if(var == 'temperature'){
+    pal = oce.colorsTemperature()
+    lab = 'Temperature [deg C]'
+    # zlim = c(-1,25)
+  } else if(var == 'salinity'){
+    pal = oce.colorsSalinity()
+    lab = 'Salinity'
+    # zlim = c(22,35)
+  } else if(var == 'density'){
+    pal = oce.colorsDensity()
+    lab = 'Density [kg/m3]'
+    # zlim = c(1017,1028)
+  } else {
+    stop('Unknown variable! Please choose from: temperature, salinity, or density')
+  }
+  
+  # define colormap
+  c = colormap(unlist(ctd[which(colnames(ctd) == var)]), breaks=100, zlim = zlim, zclip = T, col = pal) # map values to colors
+  
+  # setup for plotting
+  m = rbind(c(1,1,1,1,1,1,1,1,1,1,1,2),
+            c(1,1,1,1,1,1,1,1,1,1,1,2),
+            c(1,1,1,1,1,1,1,1,1,1,1,2),
+            c(1,1,1,1,1,1,1,1,1,1,1,2),
+            c(1,1,1,1,1,1,1,1,1,1,1,2),
+            c(1,1,1,1,1,1,1,1,1,1,1,2))
+  
+  layout(m)
+  
+  # plot section
+  plot(ctd$time, ctd$depth, ylim = c(175,0), type = 'l', col = 'grey', 
+       ylab = 'Depth [m]', xlab = '', cex.lab = cex.all, cex.axis = cex.all)
+  mtext(paste0(lab),side = 3, line = 1,adj = 0)
+  points(ctd$time, ctd$depth, pch = 21, bg = c$zcol, cex = 2, col = NULL)
+  
+  # add palette
+  drawPalette(c$zlim, col=c$col, breaks=c$breaks, zlab = '', fullpage = T, 
+              cex.axis = cex.all, cex.lab = cex.all)
+  
+}
 
 # ui ----------------------------------------------------------------------
 
